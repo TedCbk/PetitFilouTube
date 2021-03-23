82657dfdfb24fcba11746c79e8b3a54ca67cda6e
Ted <p0599847@protonmail.com>
2021-03-23 18:06:23 +0100

    Update Channel.php

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Channel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'name',
+        'slug',
+        'description',
+        'image_filename',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
